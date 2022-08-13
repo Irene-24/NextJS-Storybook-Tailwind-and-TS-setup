fa167bbb9c971a10010344a6bd93837b1fe1e3d0
Irene-24 <okaforirenen@gmail.com>
2022-08-13 15:46:52 +0100

    added msw

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,15 @@
 import "../styles/globals.css";
+import "../src/index.css";
+
 import * as NextImage from "next/image";
+// Registers the msw addon
+import { initialize, mswDecorator } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 const OriginalNextImage = NextImage.default;
 
